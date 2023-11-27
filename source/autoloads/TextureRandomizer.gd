57cc12885bda3564621f775d8d8c9dd4c42e92ed
OrangeMonster <91896426+omthegg@users.noreply.github.com>
2023-11-28 00:38:12 +0330

    random texture generator progress

--- source/autoloads/TextureRandomizer.gd
+++ source/autoloads/TextureRandomizer.gd
@@ -1,39 +1,54 @@
 extends Node
 
 var icon = preload("res://godot.png")
+var floor1 = preload("res://textures/floor1.png")
 
 var rects = []
 
 func generate_random_texture():
+	var repeating_texture = Image.new()
+	repeating_texture.create(1024, 1024, true, Image.FORMAT_RGB8)
+	for x in range(0, 1024, 64):
+		for y in range(0, 1024, 64):
+			repeating_texture.blit_rect(floor1.get_data(), Rect2(0, 0, 64, 64), Vector2(x, y))
+	
+	repeating_texture.save_png("C:/Users/EXO/Desktop/repeating_texture.png")
+	
 	randomize()
 	var r = Rect2()
 	r.position = Vector2(0, 0)
-	r.size = Vector2(128, 128)
+	r.size = Vector2(512, 512)
 	rects.append(r)
 	var x_or_y = 1
-	for i in range(8):
-		var img = Image.new()
-		img.create(128, 128, true, Image.FORMAT_RGB8)
-		for rect in rects:
+	for i in range(100):
+		#var img = Image.new()
+		#img.create(128, 128, true, Image.FORMAT_RGB8)
+		#for rect in rects:
 		#	print(rect)
-			img.fill_rect(rect, Color(rand_range(0.0, 1.0), rand_range(0.0, 1.0), rand_range(0.0, 1.0)))
+		#	img.fill_rect(rect, Color(rand_range(0.0, 1.0), rand_range(0.0, 1.0), rand_range(0.0, 1.0)))
 		
-		img.save_png(str("C:/Users/EXO/Desktop/", str(i), ".png"))
+		#img.save_png(str("C:/Users/EXO/Desktop/", str(i), ".png"))
 		
 		#var sorted_bigger_rects = rects.duplicate(true)
 		#sorted_bigger_rects.sort()
 		var sorted_bigger_rects = sort_rects_array(rects.duplicate())
 		if sorted_bigger_rects.size() > 2:
-			sorted_bigger_rects.resize(int(sorted_bigger_rects.size()/2))
+			sorted_bigger_rects.resize(int(sorted_bigger_rects.size()/1.4))
 		
 		var random_rect = sorted_bigger_rects[randi()%sorted_bigger_rects.size()]
 		
 		#x_or_y *= -1
-		x_or_y = random_rect.size.y > random_rect.size.x
+		#x_or_y = random_rect.size.y > random_rect.size.x
+		if random_rect.size.y == random_rect.size.x:
+			x_or_y = randi() %2
+		elif random_rect.size.y > random_rect.size.x:
+			x_or_y = 1
+		else:
+			x_or_y = 0
 		
 		var slice1 = random_rect
 		#slice1.position = random_rect.position
-		if x_or_y:
+		if x_or_y == 1:
 			slice1.size *= Vector2(1.0, 0.5)
 		else:
 			slice1.size *= Vector2(0.5, 1.0)
@@ -58,10 +73,21 @@ func generate_random_texture():
 	#print(sorted_bigger_rects)
 	
 	var img = Image.new()
-	img.create(128, 128, true, Image.FORMAT_RGB8)
+	img.create(512, 512, true, Image.FORMAT_RGB8)
+	
 	for rect in rects:
+		var randomly_scaled_texture = repeating_texture.duplicate()
+		#randomly_scaled_texture = icon.get_data()
+		#randomly_scaled_texture.resize(Vector2(64.0*rand_range(1.0, 2.0), 64.0*rand_range(1.0, 2.0)))
+		var random_size_x = stepify(1024*rand_range(1.0, 2.0), 16.0)
+		#randomize()
+		var random_size_y = stepify(1024*rand_range(1.0, 2.0), 16.0)
+		randomly_scaled_texture.resize(random_size_x, random_size_y, 0)
+		randomly_scaled_texture.crop(rect.size.x, rect.size.y)
+		#img.blit_rect(randomly_scaled_texture, Rect2(Vector2(0, 0), rect.size), rect.position)
+		img.blit_rect(randomly_scaled_texture, Rect2(Vector2(0, 0), rect.size), rect.position)
 		#print(rect)
-		img.fill_rect(rect, Color(rand_range(0.0, 1.0), rand_range(0.0, 1.0), rand_range(0.0, 1.0)))
+		#img.fill_rect(rect, Color(rand_range(0.0, 1.0), rand_range(0.0, 1.0), rand_range(0.0, 1.0)))
 	
 	img.save_png("C:/Users/EXO/Desktop/rand.png")
 
