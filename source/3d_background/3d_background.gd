f40a3f7f1033a6ae6b00a379f9409e7a835013b9
OrangeMonster <91896426+omthegg@users.noreply.github.com>
2025-08-21 11:32:59 +0330

    Menu progress

--- source/3d_background/3d_background.gd
+++ source/3d_background/3d_background.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
-@onready var multi_mesh_instance = $MultiMeshInstance3D
+@onready var multi_mesh_instance:MultiMeshInstance3D = $MultiMeshInstance3D
 
 func _ready():
-	for i in 10:
-		multi_mesh_instance.multimesh
+	for i in 32:
+		var b:Basis = Basis(Vector3(1.0, 1.0, 1.0).normalized(), randf_range(-90.0, 90.0))
+		var o:Vector3 = Vector3(randf_range(-8.0, 8.0), randf_range(-3.5, 3.5), randf_range(-3.0, -10.0))
+		var t:Transform3D = Transform3D(b, o)
+		multi_mesh_instance.multimesh.set_instance_transform(i, t)
