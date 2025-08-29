4eb078d56b9e4e38aa2e8aa90b06ea518c9e6d7b
OrangeMonster <91896426+omthegg@users.noreply.github.com>
2025-08-29 19:07:13 +0330

    Fixed size gizmos not moving with boxes

--- source/level_editor/move_tool.gd
+++ source/level_editor/move_tool.gd
@@ -11,12 +11,13 @@ func _ready() -> void:
 	global_position = get_parent().global_position
 	previous_global_position = global_position
 
+
 func _physics_process(_delta):
 	if top_level:
 		if global_position != previous_global_position:
 			var position_difference:Vector3 = global_position - previous_global_position
 			get_parent().global_position += position_difference
-			if get_parent() is CSGBox3D:
+			if get_parent().is_in_group("box"):
 				get_parent().reset_size_tools()
 				get_parent().set_middles()
 				get_parent().set_previous_middles()
