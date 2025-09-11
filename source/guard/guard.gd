2cb544c04a95fc1aa99900cfd5edd5586be7367b
OrangeMonster <91896426+omthegg@users.noreply.github.com>
2025-09-11 22:56:49 +0330

    Added guard walking animation

--- source/guard/guard.gd
+++ source/guard/guard.gd
@@ -1,10 +1,12 @@
 extends CharacterBody3D
 
 @onready var shooting_arm:Node3D = $Model/ShootingArm
+@onready var animation_player:AnimationPlayer = $AnimationPlayer
 
 var speed:float = 6.0
 
 func _physics_process(delta):
+	animation_player.play("walk")
 	var player:CharacterBody3D = Global.scene_manager.current_level.get_node_or_null("Player")
 	if player:
 		look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z))
