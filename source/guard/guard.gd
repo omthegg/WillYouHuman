45288c012bc100d51a4f61074d90e9aecfc7e2e2
OrangeMonster <91896426+omthegg@users.noreply.github.com>
2025-09-22 00:10:33 +0330

    Guard progress (?)

--- source/guard/guard.gd
+++ source/guard/guard.gd
@@ -16,21 +16,23 @@ var player:CharacterBody3D
 var speed:float = 5.0
 
 func _ready() -> void:
+	shooting_raycast.reparent(self)
 	#shooting_raycast = $Model/ShootingArm/Node3D/Revolver/HitscanComponent/RayCast3D
 	#print(shooting_raycast)
 	shooting_raycast.global_position = $Model/Head.global_position
 	
-	navigation_agent.path_desired_distance = 0.5
+	navigation_agent.path_desired_distance = 1.0
 	navigation_agent.target_desired_distance = 1.0
 	
-	random_timer.wait_time = randf_range(0.0, 1.0)
-	random_timer.start()
+	#random_timer.wait_time = randf_range(0.0, 1.0)
+	#random_timer.start()
+	move_timer.start()
 
 
 func _physics_process(_delta:float) -> void:
 	player = Global.scene_manager.current_level.player
 	if player:
-		if target_timer.time_left == 0.0 and shoot_timer.time_left == 0.0:
+		if shoot_timer.time_left == 0.0:
 			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z))
 			shooting_arm.look_at(player.head.global_position + Vector3(0, -0.3, 0.0))
 			shooting_raycast.look_at(player.head.global_position)
@@ -50,12 +52,13 @@ func find_path_to_player() -> void:
 
 
 func move_to_path() -> void:
-	var next_path_position:Vector3 = navigation_agent.get_next_path_position()
-	velocity = global_position.direction_to(next_path_position) * speed
 	if navigation_agent.is_target_reached():
 		velocity = Vector3.ZERO
 		if target_timer.time_left == 0.0 and shoot_timer.time_left == 0.0:
 			target()
+	else:
+		var next_path_position:Vector3 = navigation_agent.get_next_path_position()
+		velocity = global_position.direction_to(next_path_position) * speed
 	
 	move_and_slide()
 
